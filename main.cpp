b133214f72a3df0b06114924b354370f1b53c3cb
ThuanNguyen386 <thuansahin@gmail.com>
2020-06-19 10:50:48 +0700

    EmployeeManager

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <Employee.h>
+#include<map>
 #include <Helpper.h>
 #include <iostream>
 #include <cstring>
 
-
+#include <Employee.h>
 #include <StatusDate.h>
+#include<RollCallHistory.h>
 
 
 using namespace std;
@@ -16,43 +17,38 @@ using namespace std;
 
 int main()
 {
+    cout<<"khiem"<<endl;
+    // lấy data từ file gốc
     vector<Employee*> list;
-
-
     Employee *employee;
-
-    // phần này chỉ lấy ra số dòng trong file csv và xuống khỏi dòng đầu tiên
-    ifstream ifs("C:\\Users\\khiem\\Desktop\\ImportData.csv", ios::in);
     int n;
-    string abc;
-    n=Helpper ::numberLine(); // lấy ra số dòng
-    n=n-1;
-    ifs >> abc;
-    char ss[5];
-    ifs.getline(ss, 3);// loai bo xuong dong
-
-
-// dùng vì cần checkid // đọc ra tất cả
-    int line=1 ;
-    int *linePtr=&line;
-
-
+    n=Helpper ::numberLine("C:\\Users\\khiem\\Desktop\\Data.csv"); // lấy ra số dòng
+    ifstream ifs("C:\\Users\\khiem\\Desktop\\Data.csv", ios::in);
     for (int i=0 ; i < n ; i++){
         employee = new Employee();
-        employee->read(ifs ,list ,linePtr);
+        employee->readData(ifs,list);
         list.push_back(employee);
     }
 
 
-    int choiceSearch;
     int choice;
     do {
         cout <<"===========================================" << endl;
-        cout <<"= 1-Nhap nhan vien                        =" << endl;
-        cout <<"= 2-Tim nhan vien theo ma                 =" << endl;
+        cout <<"= 1-Nhap nhan vien " << endl;
+        cout <<"= 2-Tim nhan vien " << endl;
+        cout <<"= 3-In ra tat ca " << endl;
+        cout <<"= 4-Import nhan vien" << endl;
+        cout <<"= 5-Diem danh nhan vien" << endl;
+        cout <<"= 6-Xem lich su diem danh nhan vien" << endl;
+
+
+
+
+
+
         cout <<"= Chon khac de thoat                      =" << endl;
         cout <<"===========================================" << endl;
-        cout << "Chon chuc nang (1-2 hoac khac):" << endl;
+        cout << "Chon chuc nang (1-2-3-4-5 hoac khac):" << endl;
         cout << "Moi ban nhap lai lua chon = ";
         cin >> choice;
 
@@ -63,41 +59,82 @@ int main()
                 employee->enterEmployee(list,"C:\\Users\\khiem\\Desktop\\ImportData.csv");
                 list.push_back(employee);
 
-
                 cout << "Ban muon nhap tiep khong, an yes; neu muon thoat an phim bat ky \n" << endl;
                 cin >> a;
             }
         }
 
         else if(choice == 2){
-
-
-        employee->searchEmployee(list);
-
+            employee->searchEmployee(list);
         }
-        else if (choice ==3){
 
+
+        else if (choice ==3){
             if(list.size() == 0 ){
                 cout << "Danh sach khong co nhan vien nao";
             }else{
                 for (int i = 0; i < list.size(); i++ ){
                     list[i]->printEmployee();
-
-
                 }
             }
         }
 
         else if (choice ==4) { // tạo tất cả các file nhân viên chỉ lần đầu muốn
-            for (int i=0 ; i < list.size(); i++) {
-                std::ofstream o( "C:\\Users\\khiem\\Desktop\\fileNV\\"+list.at(i)->getId()+".csv");
+            cout << "Nhap vao dia chi file can import" <<endl;
+            string path;
+            cin>> path ;
+            path="C:\\Users\\khiem\\Desktop\\"+path;
+
+            // phần này chỉ lấy ra số dòng trong file csv và xuống khỏi dòng đầu tiên
+            ifstream ifs(path, ios::in);
+
+            string abc;
+            ifs >> abc;
+            char ss[5];
+            ifs.getline(ss, 3);// loai bo xuong dong
+
+            //đếm số dòng trong file
+            int n;
+            n=Helpper ::numberLine(path); // lấy ra số dòng
+            cout <<n <<endl;
+
+            n=n-1;
+            cout <<n <<endl;
+
+
+
+            // dùng để lấy đc dòng nào bị sai
+            int line=1 ;
+            int *linePtr=&line;
+
+            // thêm các nhân viên vào list để dễ thao tác
+
+
+
+
+            Employee *employee;
+            for (int i=0 ; i < n ; i++){
+                employee = new Employee();
+                employee->readImport(ifs ,list ,linePtr);
+               // cout<<employee->getId()<<endl;
+                list.push_back(employee);
             }
+
+            ifs.close();
+
 
         }
         else if(choice ==5){ // lưu thông tin điểm danh
             // nhập vào id , nhập vào ngày muốn điểm danh ,nhập vào trạng thái muốn điểm danh
-            StatusDate::read(list);
+            StatusDate::addOrUpdateStatus(list);
 
+        }
+        else if(choice == 6){
+            map<string ,string> map1;
+            map1 =RollCallHistory::filterByMonth(list);
+            for (map<string ,string> :: iterator it =map1.begin(); it !=map1.end(); it++) {
+                cout<<it->first <<", " << it->second<<endl;
+            }
         }
         else{
             break;
